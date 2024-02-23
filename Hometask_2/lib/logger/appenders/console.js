ee7fb4a856dafdf6a3e6920f8022893ab6734678
PaliAlex <ol.palinchak@gmail.com>
2024-02-23 12:58:05 +0400

    hometask_2 - added ability to read from file, write logs to file, expand level amount

--- Hometask_2/lib/logger/appenders/console.js
+++ Hometask_2/lib/logger/appenders/console.js
@@ -1,10 +1,33 @@
+import * as fs from "fs";
+import {level as levelConstant} from "../constants.js";
+
 function log(date, level, category, message) {
     console.log(formatMessage(date, level, category, message));
 }
 
 function formatMessage(date,level, category, message) {
-    return `Date: ${date}, category:${category}, level:${level}, message:${JSON.stringify(message)}`;
+    const formattedMessage = `Date: ${date}, category:${category}, level:${level}, message:${JSON.stringify(message)}`
+
+    level === levelConstant.ERROR
+        ? writeToFile('app_error.log', formattedMessage)
+        : writeToFile('app.log', formattedMessage)
+
+    return formattedMessage;
 }
 
 
-export default {log}
+function checkFileExists(filePath) {
+    try {
+        fs.accessSync(filePath, fs.constants.F_OK);
+        return true; // File exists
+    } catch (err) {
+        return false; // File does not exist
+    }
+}
+
+function writeToFile(filePath, message) {
+    const messageToFile = checkFileExists(filePath) ? `\n${message}` : message;
+
+    fs.writeFileSync(filePath, messageToFile, {flag: 'a+'})
+}
+export default {log};
