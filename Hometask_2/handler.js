205231b3e9eba8223d4efb64330c5c277be0c75d
PaliAlex <ol.palinchak@gmail.com>
2024-03-03 21:42:07 +0400

    hometask_3 - fourth commit

--- Hometask_2/handler.js
+++ Hometask_2/handler.js
@@ -3,8 +3,8 @@ import {messageFormat} from "./lib/logger/constants.js";
 
 const log = logger.getLogger("handler.js", messageFormat.JSON);
 function add(a, b) {
-    log.info(["First operand " + a]);
-    log.info(["Second operand " + b]);
+    log.info("First operand " + a);
+    log.info("Second operand " + b);
     return a + b;
 }
 
